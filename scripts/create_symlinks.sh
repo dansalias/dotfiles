c0b728d2dcef8f1d4e23e0a71bf4b140fe370a3d
d4n13ly <kiwidan92@gmail.com>
2017-11-24 22:07:24 +1300

    Update symlinks script

--- scripts/create_symlinks.sh
+++ scripts/create_symlinks.sh
@@ -1,9 +1,23 @@
-#!/bin/bash -x
-DF=$HOME/projects/dotfiles
-ln -sf $DF/zsh/.zshrc $HOME
-ln -sf $DF/vim/.vimrc $HOME
-ln -sf $DF/tmux/.tmux.conf $HOME
-ln -sf $DF/git/.gitconfig $HOME
-ln -sf $DF/gnome/user-dirs.conf $HOME/.config
-ln -sf $DF/gnome/user-dirs.dirs $HOME/.config
-ln -sf $DF/gnome/bookmarks $HOME/.config/gtk-3.0
+#!/bin/bash
+
+DF=~/projects/dotfiles
+
+# Create directories
+mkdir -p ~/.config/{i3,polybar}
+
+# x config
+ln -sf $DF/x/.xinitrc ~/.xinitrc
+ln -sf $DF/x/.Xresources ~/.Xresources
+
+# window manager config
+ln -sf $DF/i3/config ~/.config/i3/config
+ln -sf $DF/compton/compton.conf ~/.config/compton.conf
+ln -sf $DF/polybar/config ~/.config/polybar/config
+ln -sf $DF/polybar/launch.sh ~/.config/polybar/launch.sh
+ln -sf $DF/rofi/config ~/.config/rofi/config
+
+# other
+ln -sf $DF/zsh/.zshrc ~/.zshrc
+ln -sf $DF/vim/.vimrc ~/.vimrc
+ln -sf $DF/vim/city-lights.vim ~/.vim/colors/city-lights.vim
+ln -sf $DF/git/.gitconfig ~/.gitconfig
