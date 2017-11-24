a2d5c23a68728f51b2c4535afdc55b13aa64da32
d4n13ly <kiwidan92@gmail.com>
2017-11-24 22:07:48 +1300

    Remove Gnome related files

--- scripts/install_packages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-PACKAGE_LIST_LOC=$HOME/projects/dotfiles/os/packages.list
-
-apt-get install dselect
-dselect update
-dpkg --set-selections < $PACKAGE_LIST_LOC
-apt-get dselect-upgrade
--- scripts/install_vim_plug.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -x
-wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -P $HOME/.vim/autoload
