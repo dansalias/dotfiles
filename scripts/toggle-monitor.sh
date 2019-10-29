ddb6e1d671bb93413521ba225af665cd9a7f915b
dansalias <danyoungnz@gmail.com>
2019-10-29 16:53:47 +0100

    Add monitor toggle script

---
+++ scripts/toggle-monitor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+INTERNAL=eDP1
+EXTERNAL=DP1
+
+if xrandr | grep "$EXTERNAL disconnected"; then
+  xrandr --output "$EXTERNAL" --off --output "$INTERNAL" --auto
+else
+  xrandr --output "$INTERNAL" --off --output "$EXTERNAL" --auto --scale 2x2 --panning 6880x2880 && \
+  feh --bg-fill ~/projects/dotfiles/ui/backgrounds/ny.jpg
+  ~/.config/polybar/launch.sh
+fi
