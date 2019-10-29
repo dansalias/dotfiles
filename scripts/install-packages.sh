f43f0880f3f17af8416169fc1d3275c8a67e0b21
dansalias <danyoungnz@gmail.com>
2019-10-29 16:50:30 +0100

    Update packages

---
+++ scripts/install-packages.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# create packages directory
+mkdir $HOME/packages
+
+# install git
+pacman -S git
+
+# install yay
+git clone https://aur.archlinux.org/yay.git $HOME/packages/yay
+cd $HOME/packages/yay
+makepkg -si
+
+# install packages
+yay -Syyu \
+  # system
+  pulseaudio \
+  imagemagick \
+  bluez \
+  bluez-firmware \
+  bluez-utils \
+  light \
+  zsh \
+  pass \
+  vim \
+  sudo \
+  ranger \
+  nodejs \
+  httpie \
+  neofetch \
+  # ui
+  xorg \
+  i3-gaps-next-git \
+  i3lock \
+  rxvt-unicode \
+  scrot \
+  unclutter \
+  polybar \
+  rofi \
+  ttf-inconsolata \
+  ttf-croscore \
+  pavucontrol \
+  # web
+  google-chrome \
+  firefox-developer-edition \
+  browserpass-chrome \
+  #apps
+  slack-desktop \
+  vlc \
